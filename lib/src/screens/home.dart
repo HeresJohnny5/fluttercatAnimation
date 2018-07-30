b66aec1ee82ae2bd86aef1d41ee0726630abca00
John Erickson <ericksonjohn08@hotmail.com>
2018-07-30 10:14:54 -0400

    Initial Commit.

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
+
+class Home extends StatefulWidget {
+  @override
+    State<StatefulWidget> createState() {
+      return _HomeState();
+    }
+}
+
+class _HomeState extends State<Home> {
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Animation'),
+        ),
+        body: buildAnimation(),
+      );
+    }
+
+    Widget buildAnimation() {
+      return Cat();
+    }
+}
